117c1f31bb70d4c289ac030b14a2dd070e7090b3
ParthSaini-2006 <54998045+ParthSaini-2006@users.noreply.github.com>
2020-10-01 18:20:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,12 @@ window.onload = ()=>{
         var min = date.getMinutes();
         var sec = date.getSeconds();
         var ms = Math.round(date.getMilliseconds()/10);
-        if(hr < 10){
-            hr = '0'+hr
-        }
 
-        if(min < 10){
-            min = '0'+min
+        function addzero(el){
+         return el<10?('0' + el): el
         }
-
-        if(sec < 10){
-            sec = '0'+sec
-        }
-
-        if(ms < 10){
-            ms = '0'+ms;
-        }
-        time.innerHTML = hr+' : '+min+' : '+sec + ' : ' + ms;
+        
+  time.innerHTML = `${addzero(hr)} : ${addzero(min)} : ${addzero(sec)} : ${addzero(ms)}`
     },10)
 
 }
